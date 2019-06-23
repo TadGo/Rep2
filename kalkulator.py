846c260c10ce3085f035542429a68c3a0a2b3ba5
Tadeusz <t_golembiewski@o2.pl>
2019-06-23 15:12:19 +0200

    pierwsza proba

---
+++ kalkulator.py
@@ -0,0 +1,13 @@
+def get_help():
+    print('podaj dwie liczby:')
+
+
+def dodawanie(a, b):
+    wynik = a + b
+    return wynik
+
+
+get_help()
+zm1 = int(input())
+zm2 = int(input())
+print(dodawanie(zm1, zm2))
